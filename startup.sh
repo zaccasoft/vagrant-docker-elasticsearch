8a9bfd60689f4e7d1524d35f903b53f838eb8a10
Troy Goode <troygoode@gmail.com>
2013-08-15 23:14:27 -0400

    fix

--- startup.sh
+++ startup.sh
@@ -4,6 +4,7 @@ if [ -f /vagrant/Dockerfile ]; then
 
   if [[ $(sudo docker images) != *troygoode\/elastic-search* ]]; then
     # only run if `docker images` says this image hasn't been created yet
+    cd /vagrant
     sudo docker build -t troygoode/elastic-search .
   fi
 
