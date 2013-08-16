499d7ffcdc91372a7c92eae4e34937858d87a2a6
Troy Goode <troygoode@gmail.com>
2013-08-15 22:44:17 -0400

    initial commit

---
+++ startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -f /vagrant/Dockerfile ]; then
+
+  if [[ $(sudo docker images) != *troygoode\/elastic-search* ]]; then
+    # only run if `docker images` says this image hasn't been created yet
+    sudo docker build -t troygoode/elastic-search .
+  fi
+
+  if [[ $(sudo docker ps) != *troygoode\/elastic-search* ]]; then
+    # only run if not already running
+    sudo docker run -d -i -p 49000:9200 troygoode/elastic-search
+  fi
+fi
